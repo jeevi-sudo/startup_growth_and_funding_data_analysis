a98ce190d6b540afd6e3b3eb0cbe54f50e799c5e
jeevi-sudo <t.jeevitha59@gmail.com>
2026-06-04 10:50:30 +0530

    Update 2_Industry_Analysis.py

--- pages/2_Industry_Analysis.py
+++ pages/2_Industry_Analysis.py
@@ -1,1 +1,31 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+st.title("🏭 Industry Analysis")
+
+df = pd.read_csv("data/startup_data.csv")
+
+industry = df.groupby("Industry").agg({
+    "Funding Amount (M USD)":"mean",
+    "Valuation (M USD)":"mean",
+    "Revenue (M USD)":"mean"
+}).reset_index()
+
+fig = px.bar(
+    industry,
+    x="Industry",
+    y="Revenue (M USD)",
+    title="Average Revenue by Industry"
+)
+
+st.plotly_chart(fig, use_container_width=True)
+
+fig2 = px.box(
+    df,
+    x="Industry",
+    y="Valuation (M USD)",
+    title="Valuation Distribution by Industry"
+)
+
+st.plotly_chart(fig2, use_container_width=True)
