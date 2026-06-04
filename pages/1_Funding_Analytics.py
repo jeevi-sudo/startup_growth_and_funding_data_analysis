d83996db7b6f6bd2ea7cb87eba774f5cdd2057a5
jeevi-sudo <t.jeevitha59@gmail.com>
2026-06-04 10:48:05 +0530

    Update 1_Funding_Analytics.py

--- pages/1_Funding_Analytics.py
+++ pages/1_Funding_Analytics.py
@@ -1,1 +1,34 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+st.title("💰 Funding Analytics")
+
+df = pd.read_csv("data/startup_data.csv")
+
+# Funding Distribution
+
+fig = px.histogram(
+    df,
+    x="Funding Amount (M USD)",
+    nbins=30,
+    title="Funding Distribution"
+)
+
+st.plotly_chart(fig, use_container_width=True)
+
+# Top Funded Startups
+
+top = df.nlargest(
+    10,
+    "Funding Amount (M USD)"
+)
+
+fig2 = px.bar(
+    top,
+    x="Startup Name",
+    y="Funding Amount (M USD)",
+    title="Top 10 Funded Startups"
+)
+
+st.plotly_chart(fig2, use_container_width=True)
